08776525bd2471306e4963dc928b6084e72418c1
MalleLauritsen <amalie@familienlauritsen.dk>
2020-03-02 22:14:50 +0100

    add to 'javascript', 'kurv' and 'styling'
    'javascript.js', 'kurv.html' & 'styling.css'

--- javascript.js
+++ javascript.js
@@ -1,9 +1,29 @@
 //VARIABLES
 // FUNCTIONS
-function minusKnap() {
+
+function minusKnap1() {
     document.getElementById("kurvproduktantal1").value = Number(document.getElementById("kurvproduktantal1").value) - 1;
+    document.getElementById("produktpris1").value = Number(document.getElementById("produktpris1").value) / 2;
+}
+function plusKnap1() {
+    document.getElementById("kurvproduktantal1").value = Number(document.getElementById("kurvproduktantal1").value) + 1;
+    document.getElementById("produktpris1").value = Number(document.getElementById("produktpris1").value) * 2;
 }
 
-function plusKnap() {
-    document.getElementById("kurvproduktantal1").value = Number(document.getElementById("kurvproduktantal1").value) + 1;
+function minusKnap2() {
+    document.getElementById("kurvproduktantal2").value = Number(document.getElementById("kurvproduktantal2").value) - 1;
+    document.getElementById("produktpris2").value = Number(document.getElementById("produktpris2").value) / 2 ;
+}
+function plusKnap2() {
+    document.getElementById("kurvproduktantal2").value = Number(document.getElementById("kurvproduktantal2").value) + 1;
+    document.getElementById("produktpris2").value = Number(document.getElementById("produktpris2").value) * 2 ;
+}
+
+function minusKnap3() {
+    document.getElementById("kurvproduktantal3").value = Number(document.getElementById("kurvproduktantal3").value) - 1;
+        document.getElementById("produktpris3").value = Number(document.getElementById("produktpris3").value) / 2 ;
+}
+function plusKnap3() {
+    document.getElementById("kurvproduktantal3").value = Number(document.getElementById("kurvproduktantal3").value) + 1;
+    document.getElementById("produktpris3").value = Number(document.getElementById("produktpris3").value) * 2 ;
 }
