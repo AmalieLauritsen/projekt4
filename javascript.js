01b4e3647590da114d4f44a8bf49d471b7965bcf
MalleLauritsen <amalie@familienlauritsen.dk>
2020-03-02 20:16:02 +0100

    add 'kurv.png', javascript, kontakt, styling
    'kurv.png', 'javascript.js', 'kontakt.html', 'kurv.html', styling.css'

---
+++ javascript.js
@@ -0,0 +1,9 @@
+//VARIABLES
+// FUNCTIONS
+function minusKnap() {
+    document.getElementById("kurvproduktantal1").value = Number(document.getElementById("kurvproduktantal1").value) - 1;
+}
+
+function plusKnap() {
+    document.getElementById("kurvproduktantal1").value = Number(document.getElementById("kurvproduktantal1").value) + 1;
+}
